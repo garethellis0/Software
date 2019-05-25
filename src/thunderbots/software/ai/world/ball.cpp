f9c347171b3c35fe6ed4d9f7cdbd3a56015cc0d0
Gareth Ellis <gareth.ellis0@gmail.com>
2019-05-24 21:44:52 -0700

    Revert "Add cirular buffers to store ball history (#569)"
    This reverts commit 8d8fc978ed5521254845e353db188b6d68dafaf6.

--- src/thunderbots/software/ai/world/ball.cpp
+++ src/thunderbots/software/ai/world/ball.cpp
@@ -1,12 +1,8 @@
 #include "ball.h"
 
-Ball::Ball(Point position, Vector velocity, const Timestamp &timestamp,
-           unsigned int history_duration)
-    : positions_(history_duration),
-      velocities_(history_duration),
-      last_update_timestamps(history_duration)
+Ball::Ball(Point position, Vector velocity, const Timestamp &timestamp)
+    : position_(position), velocity_(velocity), last_update_timestamp(timestamp)
 {
-    addStateToBallHistory(position, velocity, timestamp);
 }
 
 void Ball::updateState(const Ball &new_ball_data)
@@ -18,24 +14,26 @@ void Ball::updateState(const Ball &new_ball_data)
 void Ball::updateState(const Point &new_position, const Vector &new_velocity,
                        const Timestamp &timestamp)
 {
-    if (timestamp < lastUpdateTimestamp())
+    if (timestamp < last_update_timestamp)
     {
         throw std::invalid_argument(
             "Error: State of ball is updating times from the past");
     }
 
-    addStateToBallHistory(new_position, new_velocity, timestamp);
+    position_             = new_position;
+    velocity_             = new_velocity;
+    last_update_timestamp = timestamp;
 }
 
 void Ball::updateStateToPredictedState(const Timestamp &timestamp)
 {
-    if (timestamp < lastUpdateTimestamp())
+    if (timestamp < last_update_timestamp)
     {
         throw std::invalid_argument(
             "Error: Predicted state is updating times from the past");
     }
 
-    auto duration_in_future = timestamp - lastUpdateTimestamp();
+    auto duration_in_future = timestamp - last_update_timestamp;
     Point new_position      = estimatePositionAtFutureTime(duration_in_future);
     Point new_velocity      = estimateVelocityAtFutureTime(duration_in_future);
 
@@ -44,12 +42,12 @@ void Ball::updateStateToPredictedState(const Timestamp &timestamp)
 
 Timestamp Ball::lastUpdateTimestamp() const
 {
-    return last_update_timestamps.front();
+    return last_update_timestamp;
 }
 
 Point Ball::position() const
 {
-    return positions_.front();
+    return position_;
 }
 
 Point Ball::estimatePositionAtFutureTime(const Duration &duration_in_future) const
@@ -64,12 +62,12 @@ Point Ball::estimatePositionAtFutureTime(const Duration &duration_in_future) con
     // real-world behavior. Position prediction should be improved as outlined in
     // https://github.com/UBC-Thunderbots/Software/issues/47
     double seconds_in_future = duration_in_future.getSeconds();
-    return position() + (velocity().norm(seconds_in_future * velocity().len()));
+    return position_ + (velocity_.norm(seconds_in_future * velocity_.len()));
 }
 
 Vector Ball::velocity() const
 {
-    return velocities_.front();
+    return velocity_;
 }
 
 Vector Ball::estimateVelocityAtFutureTime(const Duration &duration_in_future) const
@@ -84,47 +82,12 @@ Vector Ball::estimateVelocityAtFutureTime(const Duration &duration_in_future) co
     // does not necessarily reflect real-world behavior. Velocity prediction should be
     // improved as outlined in https://github.com/UBC-Thunderbots/Software/issues/47
     double seconds_in_future = duration_in_future.getSeconds();
-    return velocity() * exp(-0.1 * seconds_in_future);
-}
-
-std::vector<Point> Ball::getPreviousPositions()
-{
-    std::vector<Point> retval{};
-    for (Point p : positions_)
-        retval.push_back(p);
-
-    return retval;
-}
-
-std::vector<Vector> Ball::getPreviousVelocities()
-{
-    std::vector<Vector> retval{};
-    for (Vector v : velocities_)
-        retval.push_back(v);
-
-    return retval;
-}
-
-std::vector<Timestamp> Ball::getPreviousTimestamps()
-{
-    std::vector<Timestamp> retval{};
-    for (Timestamp t : last_update_timestamps)
-        retval.push_back(t);
-
-    return retval;
-}
-
-void Ball::addStateToBallHistory(const Point &position, const Vector &velocity,
-                                 const Timestamp &timestamp)
-{
-    positions_.push_front(position);
-    velocities_.push_front(velocity);
-    last_update_timestamps.push_front(timestamp);
+    return velocity_ * exp(-0.1 * seconds_in_future);
 }
 
 bool Ball::operator==(const Ball &other) const
 {
-    return this->position() == other.position() && this->velocity() == other.velocity();
+    return this->position_ == other.position_ && this->velocity_ == other.velocity_;
 }
 
 bool Ball::operator!=(const Ball &other) const
