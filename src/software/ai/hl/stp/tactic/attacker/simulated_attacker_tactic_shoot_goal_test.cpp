435493327443801d1d136322f3fbaa0d664a6022
Mathew MacDougall <mmacd97@gmail.com>
2021-06-09 19:28:57 -0700

    Squashed commit of the following:
    commit 8fd37001b603ed4b662bb23d72ac1688911431e6
    Author: Mathew MacDougall <mmacd97@gmail.com>
    Date:   Tue Jun 8 21:39:43 2021 -0700
    
        uncommented tests
    
    commit cf0f5d5f9d0c23481faee6e4f23646dbdda585e9
    Author: Mathew MacDougall <mmacd97@gmail.com>
    Date:   Tue Jun 8 21:28:04 2021 -0700
    
        fixed formatting
    
    commit 045a2a4bdcec600034b6b8c649c0cbb0649aa618
    Author: Mathew MacDougall <mmacd97@gmail.com>
    Date:   Tue Jun 8 21:26:59 2021 -0700
    
        fixed tests
    
    commit 3ea9dd9aa06ac7bb736784a7f1648545e1c95c34
    Author: Mathew MacDougall <mmacd97@gmail.com>
    Date:   Tue Jun 8 20:20:55 2021 -0700
    
        fixed formatting
    
    commit caba3ed728c2b4c397a53d456d69fb484db7d24a
    Author: Mathew MacDougall <mmacd97@gmail.com>
    Date:   Tue Jun 8 20:20:31 2021 -0700
    
        Renamed SimulatorConfig to PhysicsConfig, and added a parameter to control simulation rate
    
    commit c24a6dfc84e073840133e47084c3dd01aeb7c045
    Author: Mathew MacDougall <mmacd97@gmail.com>
    Date:   Tue Jun 8 19:31:19 2021 -0700
    
        double the simulation firmware/physics update rate to help track short TrajectoryPlanner trajectories with very small time segments
    
    commit c71c01171ce697d077a3a0f17399eeb3cb195d85
    Author: Tiger <62891682+xyztiger@users.noreply.github.com>
    Date:   Sun Jun 6 17:13:29 2021 -0700
    
        Update enemy penalty play (#2101)
    
    commit 75fa403341d9157b2cd643d295064d5c0dc90a9f
    Author: Cody Li <licody2000@gmail.com>
    Date:   Sun Jun 6 12:20:18 2021 -0700
    
        Fix Grsim Flicker Bug (#2079)
    
        * make buffer bigger
    
        * fix formatting
    
        * address review comment
    
    commit 40c1bd454e494cf281a0940cfc1f4ff846be6658
    Author: Akhil Veeraghanta <31548846+akhilveeraghanta@users.noreply.github.com>
    Date:   Sat Jun 5 12:20:35 2021 -0700
    
        Fix Channel Selection Bug (#2114)
    
    commit 9e491bac4e4ed9417b0a0cf2d438188bdb28df51
    Author: Jason Lee <34253612+jsn0716@users.noreply.github.com>
    Date:   Sat Jun 5 11:24:46 2021 -0700
    
        [SSL Simulation Protocol] Firmware fixes (#2102)
    
        * Fixes
    
        * Format
    
        * Remove stdio
    
        * Fix
    
        * WIP
    
        * Revert fixes
    
        * WIP
    
        * tests pass
    
        * Move global -> local velocity conversion to followPosTrajectory

--- src/software/ai/hl/stp/tactic/attacker/simulated_attacker_tactic_shoot_goal_test.cpp
+++ src/software/ai/hl/stp/tactic/attacker/simulated_attacker_tactic_shoot_goal_test.cpp
@@ -66,8 +66,8 @@ INSTANTIATE_TEST_CASE_P(
 //                        TestUtil::createStationaryRobotStatesWithId(
 //                            {Point(2.4, 1), Point(3, 0.4), Point(3, 0.8), Point(3.1, 0.6),
 //                             Point(3.1, 1), Point(4.2, 1.2)}),
-//                        Angle::fromDegrees(210)),
-        // enemy goal blocked by enemy robots with enemy threat left
+//                        Angle::fromDegrees(245))
+//        // enemy goal blocked by enemy robots with enemy threat left
 //        std::make_tuple(BallState(Point(2, 1), Vector()), Point(1, 1),
 //                        TestUtil::createStationaryRobotStatesWithId(
 //                            {Point(1.6, 1), Point(3, 0.4), Point(3, 0.8), Point(3.1, 0.6),
